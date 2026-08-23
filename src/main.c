d197c58213a6ba9549a6caf4d7454e6830999945
Ishav Sohal <ishav.sohal@mail.utoronto.ca>
2026-08-23 14:12:40 -0400

    add input and output file redirection

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <ctype.h>
+#include <fcntl.h>
 
 // Array of char pointers. A string is a char pointer, so this is an array of strings
 const char *SUPPORTED_COMMANDS[] = {"ls", "cat", "cd", "echo", "pwd", "grep"};
@@ -13,16 +14,85 @@ const int NUM_SUPPORTED = sizeof(SUPPORTED_COMMANDS) / sizeof(char *);
 const int MAX_COMMAND = 50; // Max number of tokens for a single command
 const char *COMMAND_DELIMITER = " ";
 const char *PIPE_DELIMITER = "|";
+const char *OUTPUT_REDIR_DELIM = ">";
+const char *OUTPUT_REDIR_DELIM2 = ">>";
+const char *INPUT_REDIR_DELIM = "<";
 pid_t parent_id; 
 
 
 // Used to run an individual command. This is executed by a child process, so no need to fork in here
-// NOTE: processes than run this command will have stdout redirected to the write end of a pipe (except for 
+// NOTE: processes than run this command MAY have stdout redirected to the write end of a pipe (except for 
 // the last such process). So, we cannot print to stdout here, only to stderr in the case of errors
 void run_command(char* command_str){ 
-    fprintf(stderr, "\nrun_command()");
-    fprintf(stderr,"\n%s\n", command_str);
-    fprintf(stderr, "Process ID: %d\n", getpid());
+    //fprintf(stderr, "\nrun_command()");
+    //fprintf(stderr,"\n%s\n", command_str);
+    //fprintf(stderr, "Process ID: %d\n", getpid());
+
+    // We first split on >>, in case there is output redirection occuring. 
+    char *output_redir = strstr(command_str, OUTPUT_REDIR_DELIM2);
+    char *output_file = NULL;
+    bool append = false;
+    
+    if (output_redir != NULL){
+        *output_redir = '\0'; // dereference pointer to access the char value in command_str
+        output_file = output_redir + strlen(OUTPUT_REDIR_DELIM2);
+
+        // Remove any leading whitespace between the >> and the output file name
+        while (output_file != NULL && isspace(*output_file)){
+            output_file++;
+        }
+
+        // Remove any trailing whitespace from the output file name
+        char *end = output_file + strlen(output_file) - 1;
+        while (isspace(*end)){
+            *end = '\0';
+            end -= 1;
+        }
+
+        append = true;
+    } 
+    // If >> was not found in the command, we can go ahead and check for >
+    else if ((output_redir = strchr(command_str, OUTPUT_REDIR_DELIM[0])) != NULL) {
+        *output_redir = '\0'; // dereference pointer to access the char value in command_str
+        output_file = output_redir + 1;
+
+        // Remove any leading whitespace between the >> and the output file name
+        while (output_file != NULL && isspace(*output_file)){
+            output_file++;
+        }
+
+        // Remove any trailing whitespace from the output file name
+        char *end = output_file + strlen(output_file) - 1;
+        while (isspace(*end)){
+            *end = '\0';
+            end -= 1;
+        }
+    }  
+
+    // We also split on <, in case there is input redirection occuring. Note that its possible for both input
+    // and output redirection to occur.
+    char *input_redir = strchr(command_str, INPUT_REDIR_DELIM[0]);
+    char *input_file = NULL;
+
+    if (input_redir != NULL){
+        *input_redir = '\0';
+        input_file = command_str;
+        command_str = input_redir + 1;
+
+        // Remove any trailing whitespace between the input file name and the <
+        char *end = input_file + strlen(input_file) - 1;
+        while (isspace(*end)){
+            *end = '\0';
+            end -= 1;
+        }
+
+        // Remove any leading whitespace from the input file name
+        while (input_file != NULL && isspace(*input_file)){
+            input_file++;
+        }
+    }
+
+
     // Parse user input, determine which valid command was executed, if any
     char *token_ptr;
     char *curr_token = strtok_r(command_str, COMMAND_DELIMITER, &token_ptr);
@@ -48,7 +118,7 @@ void run_command(char* command_str){
     command[0] = curr_token;
     int currSize = 1;
 
-    
+    // strtok will ignore contiguous occurrances of " " between tokens, as well as trailing whitespace
     while (curr_token != NULL && currSize < MAX_COMMAND){
         curr_token = strtok_r(NULL, COMMAND_DELIMITER, &token_ptr);
         command[currSize] = curr_token;
@@ -70,15 +140,48 @@ void run_command(char* command_str){
         exit(0);
     }
 
-    // The child process calls exec to run the command
+    // First, we redirect stdout to the specified file if the user chose to do so.
+    if (output_file != NULL){
+        int flags = O_WRONLY | O_CREAT;
+
+        if (append){
+            flags = flags | O_APPEND;
+        } else{
+            flags = flags | O_TRUNC;
+        }
+        
+        int fd = open(output_file, flags, S_IRWXU);
+
+        if (fd == -1){
+            perror("\nFailed to open output file");
+            return;
+        }
+
+        dup2(fd, STDOUT_FILENO);
+    }
+
+    // We also redirect stdin to the specified file if the user chose to do so
+    if (input_file != NULL){
+        int fd = open(input_file, O_RDONLY, S_IRWXU);
+
+        if (fd == -1){
+            perror("\nFailed to open input file");
+            return;
+        }
+
+        dup2(fd, STDIN_FILENO);
+    }
+
+
     execvp(command[0], command);
 }
 
+
 // Run a command with two or more commands piped together
 void run_piped(char* piped_command){
-    fprintf(stderr, "\nrun_piped()");
-    fprintf(stderr,"\n%s", piped_command);
-    fprintf(stderr,"\nProcess ID: %d", getpid());
+    //fprintf(stderr, "\nrun_piped()");
+    //fprintf(stderr,"\n%s", piped_command);
+    //fprintf(stderr,"\nProcess ID: %d", getpid());
 
     // If there are pipes in the command, we must split on then, and run each command seperately
     // We split on the first occurance of |. We execute the first part as it, and recursively process the 
@@ -93,17 +196,11 @@ void run_piped(char* piped_command){
         command2 = first_pipe + 1;
     }
 
-    fprintf(stderr,"\ncommand 1: %s", command1);
+    //fprintf(stderr,"\ncommand 1: %s", command1);
     while (command2 != NULL && isspace(*command2)){
         command2++;
-        fprintf(stderr,"\ncommand 2: %s", command2);
+        //fprintf(stderr,"\ncommand 2: %s", command2);
     }   
-    
-    // if (command2 != NULL){
-    //     run_piped(command2);
-    // }
-
-    // return;
 
     // If there is only one command, just fork one time and run the command.
     // Forking is only necessary if the (original) parent is calling this function. Otherwise, if its a child,
@@ -127,7 +224,7 @@ void run_piped(char* piped_command){
                 pid_t res = waitpid(pid, &status, 0);
 
                 if (res == -1){
-                    perror("\nChild process 1 failed");
+                    perror("\nChild process failed");
                 }
             }
             return;
@@ -170,7 +267,7 @@ void run_piped(char* piped_command){
         pid_t res = waitpid(pid, &status, 0);    
     
         if (res == -1){
-            perror("\nChild process 1 failed");
+            perror("\nChild process failed");
         }
         
         pid_t pid2 = fork();
@@ -208,17 +305,18 @@ void run_piped(char* piped_command){
     return;
 }
 
+
 // Prompt user to enter a command
 // Each call to run() exists on a seperate stack frame. However, each stack frame for a run() call is placed in the same 
 // location in the stack (right above main()), so char x[50] is also placed in the same location as the previous call to 
 // run(). If fgets reads nothing and hits an EOF, x will still point to the same data as the previous run, which would be the
 // previous-most command that was processed. 
 void run() {
-    fprintf(stderr,"\nrun()\n");
+    //fprintf(stderr,"\nrun()\n");
     parent_id = getpid();
-    fprintf(stderr,"Process ID: %d\n", parent_id);
+    //fprintf(stderr,"Process ID: %d\n", parent_id);
     char x[100];
-    fprintf(stderr,"mysh> ");
+    printf("mysh> ");
     char * res = fgets(x, sizeof(x), stdin);
 
     // If there is no input, return to calling location, which will reprompt user for input
@@ -232,13 +330,12 @@ void run() {
 }
 
 
-
 int main () {
     int x = 0;
     while (x < 10){
         run();
         x += 1;
     }
-    
+
     return 0;
 }
